c6e2856c66f5f6bdb61894ba5e02aa4b7aad28eb
podumai <kirillsm05@gmail.com>
2025-08-04 11:06:53 +0700

    Added new benchmarks.

--- benchmark/bits_dbitset.cpp
+++ benchmark/bits_dbitset.cpp
@@ -1,65 +1,100 @@
 #include <benchmark/benchmark.h>
 #include <dynamic_bitset/dynamic_bitset.hpp>
+#include <dynamic_bitset/benchmark/benchmark.hpp>
 
-namespace
-{
+#define BITS_DB(type) bits::DynamicBitset<type>
+#define BITS_CONST_DB(type) const bits::DynamicBitset<type>
 
-auto EmptyTest(benchmark::State& state) -> void
-{
-  for (auto _ : state)
-  {
-    bits::DynamicBitset unit;
-  }
-}
+BITS_DefaultConstructorBenchmark(BITS_DB(unsigned char), DynamicBitset());
+BITS_DefaultConstructorBenchmark(BITS_DB(unsigned short), DynamicBitset());
+BITS_DefaultConstructorBenchmark(BITS_DB(unsigned), DynamicBitset());
+BITS_DefaultConstructorBenchmark(BITS_DB(unsigned long), DynamicBitset());
+BITS_DefaultConstructorBenchmark(BITS_DB(unsigned long long), DynamicBitset());
 
-auto CopyTest(benchmark::State& state) -> void
-{
-  bits::DynamicBitset unit{1'000'000};
-  for (auto _ : state)
-  {
-    bits::DynamicBitset copiedUnit{unit};
-  }
-}
+BITS_CopyConstructorBenchmark(BITS_DB(unsigned char), DynamicBitset(DynamicBitset&));
+BITS_CopyConstructorBenchmark(BITS_DB(unsigned short), DynamicBitset(DynamicBitset&));
+BITS_CopyConstructorBenchmark(BITS_DB(unsigned), DynamicBitset(DynamicBitset&));
+BITS_CopyConstructorBenchmark(BITS_DB(unsigned long), DynamicBitset(DynamicBitset&));
+BITS_CopyConstructorBenchmark(BITS_DB(unsigned long long), DynamicBitset(DynamicBitset&));
 
-auto EmptyPushBackTest(benchmark::State& state) -> void
-{
-  for (auto _ : state)
-  {
-    bits::DynamicBitset unit;
-    for (int i{}; i < 1'000'000; ++i)
-    {
-      unit.push_back(!(i & 1));
-    }
-  }
-}
+BITS_MoveConstructorBenchmark(BITS_DB(unsigned char), DynamicBitset(DynamicBitset&&));
+BITS_MoveConstructorBenchmark(BITS_DB(unsigned short), DynamicBitset(DynamicBitset&&));
+BITS_MoveConstructorBenchmark(BITS_DB(unsigned), DynamicBitset(DynamicBitset&&));
+BITS_MoveConstructorBenchmark(BITS_DB(unsigned long), DynamicBitset(DynamicBitset&&));
+BITS_MoveConstructorBenchmark(BITS_DB(unsigned long long), DynamicBitset(DynamicBitset&&));
 
-auto SubscriptTraverseTest(benchmark::State& state) -> void
-{
-  bits::DynamicBitset unit(1'000'000);
-  for (auto _ : state)
-  {
-    for (int i{}; i < 1'000'000; ++i)
-    {
-      unit[i];
-    }
-  }
-}
+BITS_CopyAssignmentBenchmark(BITS_DB(unsigned char), operator=);
+BITS_CopyAssignmentBenchmark(BITS_DB(unsigned short), operator=);
+BITS_CopyAssignmentBenchmark(BITS_DB(unsigned), operator=);
+BITS_CopyAssignmentBenchmark(BITS_DB(unsigned long), operator=);
+BITS_CopyAssignmentBenchmark(BITS_DB(unsigned long long), operator=);
 
-auto BitwiseANDoperatorTest(benchmark::State& state) -> void
-{
-  bits::DynamicBitset unit(1'000'000);
-  bits::DynamicBitset anotherUnit(1'000'000);
-  for (auto _ : state)
-  {
-    unit &= anotherUnit;
-  }
-}
+BITS_MoveAssignmentBenchmark(BITS_DB(unsigned char), operator=);
+BITS_MoveAssignmentBenchmark(BITS_DB(unsigned short), operator=);
+BITS_MoveAssignmentBenchmark(BITS_DB(unsigned), operator=);
+BITS_MoveAssignmentBenchmark(BITS_DB(unsigned long), operator=);
+BITS_MoveAssignmentBenchmark(BITS_DB(unsigned long long), operator=);
 
-}
+BITS_PushBackBenchmark(BITS_DB(unsigned char), push_back, BITS_DefaultRangeGenerator);
+BITS_PushBackBenchmark(BITS_DB(unsigned char), push_back, BITS_DefaultDenseRangeGenerator);
+BITS_PushBackBenchmark(BITS_DB(unsigned short), push_back, BITS_DefaultRangeGenerator);
+BITS_PushBackBenchmark(BITS_DB(unsigned short), push_back, BITS_DefaultDenseRangeGenerator);
+BITS_PushBackBenchmark(BITS_DB(unsigned), push_back, BITS_DefaultRangeGenerator);
+BITS_PushBackBenchmark(BITS_DB(unsigned), push_back, BITS_DefaultDenseRangeGenerator);
+BITS_PushBackBenchmark(BITS_DB(unsigned long), push_back, BITS_DefaultRangeGenerator);
+BITS_PushBackBenchmark(BITS_DB(unsigned long), push_back, BITS_DefaultDenseRangeGenerator);
+BITS_PushBackBenchmark(BITS_DB(unsigned long long), push_back, BITS_DefaultRangeGenerator);
+BITS_PushBackBenchmark(BITS_DB(unsigned long long), push_back, BITS_DefaultDenseRangeGenerator);
 
-BENCHMARK(EmptyTest);
-BENCHMARK(CopyTest);
-BENCHMARK(EmptyPushBackTest);
-BENCHMARK(SubscriptTraverseTest);
-BENCHMARK(BitwiseANDoperatorTest);
+BITS_SubscriptLoopBenchmark(BITS_DB(unsigned char), operator[]);
+BITS_SubscriptLoopBenchmark(BITS_CONST_DB(unsigned char), operator[]);
+BITS_SubscriptLoopBenchmark(BITS_DB(unsigned short), operator[]);
+BITS_SubscriptLoopBenchmark(BITS_CONST_DB(unsigned short), operator[]);
+BITS_SubscriptLoopBenchmark(BITS_DB(unsigned), operator[]);
+BITS_SubscriptLoopBenchmark(BITS_CONST_DB(unsigned), operator[]);
+BITS_SubscriptLoopBenchmark(BITS_DB(unsigned long), operator[]);
+BITS_SubscriptLoopBenchmark(BITS_CONST_DB(unsigned long), operator[]);
+BITS_SubscriptLoopBenchmark(BITS_DB(unsigned long long), operator[]);
+BITS_SubscriptLoopBenchmark(BITS_CONST_DB(unsigned long long), operator[]);
+
+BITS_AtLoopBenchmark(BITS_DB(unsigned char), at);
+BITS_AtLoopBenchmark(BITS_CONST_DB(unsigned char), at);
+BITS_AtLoopBenchmark(BITS_DB(unsigned short), at);
+BITS_AtLoopBenchmark(BITS_CONST_DB(unsigned short), at);
+BITS_AtLoopBenchmark(BITS_DB(unsigned), at);
+BITS_AtLoopBenchmark(BITS_CONST_DB(unsigned), at);
+BITS_AtLoopBenchmark(BITS_DB(unsigned long), at);
+BITS_AtLoopBenchmark(BITS_CONST_DB(unsigned long), at);
+BITS_AtLoopBenchmark(BITS_DB(unsigned long long), at);
+BITS_AtLoopBenchmark(BITS_CONST_DB(unsigned long long), at);
+
+BITS_TestLoopBenchmark(BITS_DB(unsigned char), test);
+BITS_TestLoopBenchmark(BITS_CONST_DB(unsigned char), test);
+BITS_TestLoopBenchmark(BITS_DB(unsigned short), test);
+BITS_TestLoopBenchmark(BITS_CONST_DB(unsigned short), test);
+BITS_TestLoopBenchmark(BITS_DB(unsigned), test);
+BITS_TestLoopBenchmark(BITS_CONST_DB(unsigned), test);
+BITS_TestLoopBenchmark(BITS_DB(unsigned long), test);
+BITS_TestLoopBenchmark(BITS_CONST_DB(unsigned long), test);
+BITS_TestLoopBenchmark(BITS_DB(unsigned long long), test);
+BITS_TestLoopBenchmark(BITS_CONST_DB(unsigned long long), test);
+
+BITS_AndOperatorBenchmark(BITS_DB(unsigned char), operator&=);
+BITS_AndOperatorBenchmark(BITS_DB(unsigned short), operator&=);
+BITS_AndOperatorBenchmark(BITS_DB(unsigned), operator&=);
+BITS_AndOperatorBenchmark(BITS_DB(unsigned long), operator&=);
+BITS_AndOperatorBenchmark(BITS_DB(unsigned long long), operator&=);
+
+BITS_OrOperatorBenchmark(BITS_DB(unsigned char), operator|=);
+BITS_OrOperatorBenchmark(BITS_DB(unsigned short), operator|=);
+BITS_OrOperatorBenchmark(BITS_DB(unsigned), operator|=);
+BITS_OrOperatorBenchmark(BITS_DB(unsigned long), operator|=);
+BITS_OrOperatorBenchmark(BITS_DB(unsigned long long), operator|=);
+
+BITS_XorOperatorBenchmark(BITS_DB(unsigned char), operator^=);
+BITS_XorOperatorBenchmark(BITS_DB(unsigned short), operator^=);
+BITS_XorOperatorBenchmark(BITS_DB(unsigned), operator^=);
+BITS_XorOperatorBenchmark(BITS_DB(unsigned long), operator^=);
+BITS_XorOperatorBenchmark(BITS_DB(unsigned long long), operator^=);
+
 BENCHMARK_MAIN();
