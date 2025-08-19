65e81366873565d2a3e28a3465b601fe9f29c287
podumai <kirillsm05@gmail.com>
2025-08-19 18:37:42 +0700

    Updated benchmarks for std::vector<bool>.

--- benchmark/std/benchmark.cpp
+++ benchmark/std/benchmark.cpp
@@ -1,54 +1,45 @@
 #include <benchmark/benchmark.h>
+#include <dynamic_bitset/benchmark/benchmark.hpp>
 #include <vector>
 
-namespace
-{
+#define STD_DB typename std::vector<bool>
+#define STD_CONST_DB const typename std::vector<bool>
 
-auto EmptyTest(benchmark::State& state) -> void
-{
-  for (auto _ : state)
-  {
-    std::vector<bool> unit;
-  }
-}
+BITS_DefaultConstructorBenchmark(STD_DB, std::vector());
 
-auto CopyTest(benchmark::State& state) -> void
-{
-  std::vector<bool> unit(1'000'000);
-  for (auto _ : state)
-  {
-    std::vector<bool> copiedUnit{unit};
-  }
-}
+BITS_CopyConstructorBenchmark(STD_DB, std::vector(const std::vector<bool>&));
 
-auto EmptyPushBackTest(benchmark::State& state) -> void
-{
-  for (auto _ : state)
-  {
-    std::vector<bool> unit;
-    for (int i{}; i < 1'000'000; ++i)
-    {
-      unit.push_back(!(i & 1));
-    }
-  }
-}
+BITS_MoveConstructorBenchmark(STD_DB, std::vector(std::vector<bool>&&));
 
-auto SubscriptTraverseTest(benchmark::State& state) -> void
-{
-  std::vector<bool> unit(1'000'000);
-  for (auto _ : state)
-  {
-    for (int i{}; i < 1'000'000; ++i)
-    {
-      unit[i];
-    }
-  }
-}
+BITS_CopyAssignmentBenchmark(STD_DB, operator=);
 
-}
+BITS_MoveAssignmentBenchmark(STD_DB, operator=);
 
-BENCHMARK(EmptyTest);
-BENCHMARK(CopyTest);
-BENCHMARK(EmptyPushBackTest);
-BENCHMARK(SubscriptTraverseTest);
+BITS_PushBackBenchmark(STD_DB, push_back, BITS_DefaultRangeGenerator);
+BITS_PushBackBenchmark(STD_DB, push_back, BITS_DefaultDenseRangeGenerator);
+
+BITS_PopBackBenchmark(STD_DB, pop_back);
+
+BITS_SubscriptLoopBenchmark(STD_DB, operator[]);
+BITS_SubscriptLoopBenchmark(STD_CONST_DB, operator[]);
+
+BITS_AtLoopBenchmark(STD_DB, at);
+BITS_AtLoopBenchmark(STD_CONST_DB, at);
+
+BITS_FrontBenchmark(STD_DB, front);
+BITS_FrontBenchmark(STD_CONST_DB, front);
+
+BITS_BackBenchmark(STD_DB, back);
+BITS_BackBenchmark(STD_CONST_DB, back);
+
+BITS_EmptyBenchmark(STD_DB, empty);
+
+BITS_FlipBenchmark(STD_DB, flip);
+
+BITS_SwapBenchmark(STD_DB, swap);
+
+BITS_SizeBenchmark(STD_DB, size);
+
+BITS_CapacityBenchmark(STD_DB, capacity);
+
 BENCHMARK_MAIN();
